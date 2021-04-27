86dc0a45c91e349cf515a38c4776f691068c8ca9
www <xxx@yyy.zz>
2021-04-27 16:43:43 +0200

    Rework mklinks

--- mklinks.cmd
+++ mklinks.cmd
@@ -1,15 +1,46 @@
 @echo off
 setlocal
 
-set MACHINE=%1
-if [%MACHINE%]==[] set MACHINE=%COMPUTERNAME%
+net session 1> NUL 2>&1
+if %ERRORLEVEL% NEQ 0 (
+  echo Sorry, this batch requires elevated privileges.
+  pause
+  goto :eof
+)
 
-cd /d %~dp0..\..
+set HOSTNAME=%1
+if [%HOSTNAME%]==[] set HOSTNAME=%COMPUTERNAME%
 
-mklink .bashrc       DotFiles\git\dotfiles\.bashrc
-mklink .bash_profile DotFiles\git\dotfiles\.bash_profile
-mklink .gitconfig    DotFiles\git\dotfiles\.gitconfig
-mklink .inputrc      DotFiles\git\dotfiles\.inputrc
-mklink .minttyrc     DotFiles\git\dotfiles\.minttyrc.%MACHINE%
+set here=%~dp0
+
+pushd %here%..
+call set basedir=%%here:%CD%\=%%
+
+set srcdir=%basedir%msys2\dotfiles
+call :create_link %srcdir%\.bashrc
+call :create_link %srcdir%\.bash_profile
+call :create_link %srcdir%\.inputrc
+call :create_link %srcdir%\.minttyrc
+
+set srcdir=%basedir%git\dotfiles
+call :create_link %srcdir%\.gitconfig
+
+popd
 
 endlocal
+goto :eof
+
+:create_link
+setlocal
+if exist "%1.%COMPUTERNAME%" set target=%1.%COMPUTERNAME%
+if _%target%_==__ (
+  if exist "%1" set target=%1
+)
+if _%target%_==__ (
+  echo [*ERROR*] File not found: %1
+  exit /b
+)
+set link=%~nx1%
+if exist "%link%" del "%link%"
+mklink "%link%" "%target%"
+exit /b
